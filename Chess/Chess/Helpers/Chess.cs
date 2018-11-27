e2f1d683b0b24ccd69269e8c4bc567542230b5ac
Kim Khanh Luu <luukhanh96@gmail.com>
2018-11-27 19:47:47 +0100

    initial commit

---
+++ Chess/Chess/Helpers/Chess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chess.Helpers
+{
+    public class Chess
+    {
+    }
+
+    public class Name
+    {
+        public String rook { get { return "rook";  } }
+        public String knight { get { return "knight"; } }
+        public String bishop { get { return "bishop"; } }
+        public String queen { get { return "queen"; } }
+        public String king { get { return "king"; } }
+        public String pawn { get { return "pawn"; } }
+    }
+    public class Properties
+    {
+        Name name = new Name();
+        Color color = new Color(); 
+    }
+}
