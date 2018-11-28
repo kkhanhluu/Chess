33650f40015a90656a309eac7e1e5895f5a06f21
Kim Khanh Luu <luukhanh96@gmail.com>
2018-11-28 01:00:35 +0100

    Finish Drag and Drop and implement logic for pawn

--- Chess/Chess/Controllers/HomeController.cs
+++ Chess/Chess/Controllers/HomeController.cs
@@ -4,106 +4,207 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Chess.Models;
-using Chess.Helpers; 
+using Chess.Helpers;
 
 namespace Chess.Controllers
 {
     public class HomeController : Controller
     {
         public static ChessModel chessModel = new ChessModel();
+        public static int[,] board = new int[8, 8]; // 0: empty, 1: black, 2:white
         public ActionResult Index()
         {
             chessModel.allPieces.Clear();
-            int x1 = 2, x2 = 1;
-            String color = "black"; 
+            int x1 = 1, x2 = 0;
+            String color = "black";
+            int cell = 1;
 
             for (int i = 0; i < 2; i++)
             {
                 // white pieces
                 if (i == 1)
                 {
-                    x1 = 7;
-                    x2 = 8;
-                    color = "white"; 
+                    x1 = 6;
+                    x2 = 7;
+                    color = "white";
+                    cell = 2;
                 }
-                for (int j = 1; j <= 8; j++)
+                for (int j = 0; j < 8; j++)
                 {
                     Piece piece = new Piece(); // pawn
                     piece.pawn.color = color;
                     piece.pawn.column = j;
                     piece.pawn.row = x1;
                     piece.pawn.id = j.ToString() + color + piece.pawn.name;
+                    piece.pawn.isStart = true;
+                    board[x1, j] = cell;
                     chessModel.allPieces.Add(piece.pawn);
-                    switch(j)
+                    switch (j)
                     {
-                        case 1: // rook
+                        case 0: // rook
                             piece = new Piece();
                             piece.rook.color = color;
                             piece.rook.column = j;
                             piece.rook.row = x2;
                             piece.rook.id = j.ToString() + color + piece.rook.name;
                             chessModel.allPieces.Add(piece.rook);
+                            board[x2, j] = cell;
                             break;
-                        case 2: // knight
+                        case 1: // knight
                             piece = new Piece();
                             piece.knight.color = color;
                             piece.knight.column = j;
                             piece.knight.row = x2;
                             piece.knight.id = j.ToString() + color + piece.knight.name;
                             chessModel.allPieces.Add(piece.knight);
+                            board[x2, j] = cell;
                             break;
-                        case 3: // bishop
+                        case 2: // bishop
                             piece = new Piece();
                             piece.bishop.color = color;
                             piece.bishop.column = j;
                             piece.bishop.row = x2;
                             piece.bishop.id = j.ToString() + color + piece.bishop.name;
                             chessModel.allPieces.Add(piece.bishop);
+                            board[x2, j] = cell;
                             break;
-                        case 4: // queen
+                        case 3: // queen
                             piece = new Piece();
                             piece.queen.color = color;
                             piece.queen.column = j;
                             piece.queen.row = x2;
                             piece.queen.id = j.ToString() + color + piece.queen.name;
                             chessModel.allPieces.Add(piece.queen);
+                            board[x2, j] = cell;
                             break;
-                        case 5: // king
+                        case 4: // king
                             piece = new Piece();
                             piece.king.color = color;
                             piece.king.column = j;
                             piece.king.row = x2;
                             piece.king.id = j.ToString() + color + piece.king.name;
                             chessModel.allPieces.Add(piece.king);
+                            board[x2, j] = cell;
                             break;
-                        case 6: // bishop
+                        case 5: // bishop
                             piece = new Piece();
                             piece.bishop.color = color;
                             piece.bishop.column = j;
                             piece.bishop.row = x2;
                             piece.bishop.id = j.ToString() + color + piece.bishop.name;
                             chessModel.allPieces.Add(piece.bishop);
+                            board[x2, j] = cell;
                             break;
-                        case 7: // knight
+                        case 6: // knight
                             piece = new Piece();
                             piece.knight.color = color;
                             piece.knight.column = j;
                             piece.knight.row = x2;
                             piece.knight.id = j.ToString() + color + piece.knight.name;
                             chessModel.allPieces.Add(piece.knight);
+                            board[x2, j] = cell;
                             break;
-                        case 8: // rook
+                        case 7: // rook
                             piece = new Piece();
                             piece.rook.color = color;
                             piece.rook.column = j;
                             piece.rook.row = x2;
                             piece.rook.id = j.ToString() + color + piece.rook.name;
                             chessModel.allPieces.Add(piece.rook);
+                            board[x2, j] = cell;
                             break;
                     }
                 }
             }
             return View(chessModel);
+        }
+
+        public JsonResult Move(int targetRow, int targetColumn, String pieceId)
+        {
+            var piece = chessModel.allPieces.SingleOrDefault(x => x.id == pieceId);
+            var currentRow = piece.row;
+            var currentColumn = piece.column;
+            if (piece == null)
+            {
+                return Json(new { isMoveable = false }, JsonRequestBehavior.AllowGet);
+            }
+            // current number in chess board
+            int cell = piece.color == "black" ? 1 : 2;
+            if (board[targetRow, targetColumn] == cell)
+            {
+                return Json(new { isMoveable = false }, JsonRequestBehavior.AllowGet);
+            }
+            switch (piece.name)
+            {
+                case "pawn":
+                    // whte pawn 
+                    if (piece.color == "white")
+                    {
+                        if (piece.isStart)
+                        {
+                            if (currentRow - targetRow <= 2 && targetColumn == currentColumn)
+                            {
+                                WhitePawnUpdateBoard(targetRow, targetColumn, piece, currentRow, currentColumn);
+                                return Json(new { isMoveable = true, canDelete = false }, JsonRequestBehavior.AllowGet);
+                            }
+                        }
+                        if (targetRow - currentRow == -1 && targetColumn == currentColumn)
+                        {
+                            WhitePawnUpdateBoard(targetRow, targetColumn, piece, currentRow, currentColumn);
+                            return Json(new { isMoveable = true, canDelete = false }, JsonRequestBehavior.AllowGet);
+                        }
+                        if (targetRow - currentRow == -1 && Math.Abs(targetColumn - currentColumn) == 1 && board[targetRow, targetColumn] == 1)
+                        {
+                            WhitePawnUpdateBoard(targetRow, targetColumn, piece, currentRow, currentColumn);
+                            var deletedPiece = chessModel.allPieces.Where(x => x.row == targetRow && x.column == targetColumn && x.id != pieceId).FirstOrDefault();
+                            chessModel.allPieces.Remove(deletedPiece);
+                            return Json(new { isMoveable = true, canDelete = deletedPiece.id }, JsonRequestBehavior.AllowGet);
+                        }
+                    }
+                    if (piece.color == "black")
+                    {
+                        if (piece.isStart)
+                        {
+                            if (targetRow - currentRow <= 2 && targetColumn == currentColumn)
+                            {
+                                BlackPawnUpdateBoard(targetRow, targetColumn, piece, currentRow, currentColumn);
+                                return Json(new { isMoveable = true, canDelete = false }, JsonRequestBehavior.AllowGet);
+                            }
+                        }
+                        if (targetRow - currentRow == 1 && targetColumn == currentColumn)
+                        {
+                            BlackPawnUpdateBoard(targetRow, targetColumn, piece, currentRow, currentColumn);
+                            return Json(new { isMoveable = true, canDelete = false }, JsonRequestBehavior.AllowGet);
+                        }
+                        if (targetRow - currentRow == 1 && Math.Abs(targetColumn - currentColumn) == 1 && board[targetRow, targetColumn] == 2)
+                        {
+                            BlackPawnUpdateBoard(targetRow, targetColumn, piece, currentRow, currentColumn);
+                            var deletedPiece = chessModel.allPieces.Where(x => x.row == targetRow && x.column == targetColumn && x.id != pieceId).FirstOrDefault();
+                            chessModel.allPieces.Remove(deletedPiece);
+                            return Json(new { isMoveable = true, canDelete = deletedPiece.id }, JsonRequestBehavior.AllowGet);
+                        }
+                    }
+                    break;
+            }
+            return Json(new { isMoveable = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        private static void BlackPawnUpdateBoard(int targetRow, int targetColumn, Properties piece, int currentRow, int currentColumn)
+        {
+            board[currentRow, currentColumn] = 0;
+            board[targetRow, targetColumn] = 1;
+            piece.row = targetRow;
+            piece.column = targetColumn;
+            piece.isStart = false;
+        }
+
+        private static void WhitePawnUpdateBoard(int targetRow, int targetColumn, Properties piece, int currentRow, int currentColumn)
+        {
+            board[currentRow, currentColumn] = 0;
+            board[targetRow, targetColumn] = 2;
+            piece.row = targetRow;
+            piece.column = targetColumn;
+            piece.isStart = false;
         }
 
         public ActionResult About()
---
+++ Chess/Chess/Models/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chess.Models
+{
+    public class Cell
+    {
+        public int row { get; set; }
+        public int column { get; set; }
+    }
+}
